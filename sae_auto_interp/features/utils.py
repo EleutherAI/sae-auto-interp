08ff4e4ef4103d288d34a1d2d5973fc8ff3df148
SrGonao <g-spaulo@hotmail.com>
2024-06-27 01:50:15 +1000

    Updating feature collection by adding dataset configs

--- sae_auto_interp/features/utils.py
+++ sae_auto_interp/features/utils.py
@@ -6,30 +6,51 @@ from typing import List
 def select_features(features:Tensor, indices:Tensor, feature_index:Tensor) -> tuple[Tensor,Tensor]:
     return features[indices[:,2] == feature_index], indices[indices[:,2] == feature_index]
 
+def cluster_features(selected_features,selected_indices):
+    unique_sentences = torch.unique(selected_indices[:,0])
+    sentence_features = []
+    sum = []
+    indices = []
+    for i,sentence in enumerate(unique_sentences):
+        sentence_indices = torch.nonzero(selected_indices[:,0] == sentence)
+        selected = selected_features[sentence_indices]
+        sentence_features.append(selected.squeeze(1))
+        sum.append(selected.sum())
+        indices.append(selected_indices[sentence_indices].squeeze(1))
+    return sentence_features,sum,indices
 
 def get_activated_sentences(features:Tensor, indices:Tensor, feature_index:Tensor, all_tokens:List[Tensor],max_selection:int=50) -> tuple[Tensor,Tensor,Tensor]:
     selected_features, selected_indices = select_features(features, indices, feature_index)
-    if selected_features.shape[0] < max_selection:
-        max_selection = selected_features.shape[0]
+    sentence_features, sum,sentence_indices = cluster_features(selected_features,selected_indices)
+    
+    if len(sentence_features) < max_selection:
+        max_selection = len(sentence_features)
     else:
         max_selection = max_selection
-    top_activations = selected_features.topk(max_selection)
-    
+    if max_selection == -1:
+        top_activations = sentence_features
+        top_activation_indices = range(len(sentence_features))
+        max_selection = len(sentence_features)
+    else:
+        sum = torch.tensor(sum)
+        top_activations = sum.topk(max_selection)
+        top_activation_indices = top_activations.indices
+        
     #These are the top activations, but we want to also get other activations in the same sentences
-    top_indices = selected_indices[top_activations.indices]
     
-    unique_top_sentence_idx = top_indices[:,0].unique()
     all_activations = torch.tensor([])
     all_indices = torch.tensor([])
+    sentences = torch.tensor([],dtype=torch.int64)
     #Get all activations in the same sentences
-    for counter,idx in enumerate(unique_top_sentence_idx):
-        wanted_indexes = torch.nonzero(selected_indices[:,0] == idx)
-        activations = selected_features[wanted_indexes]
-        all_activations = torch.cat((all_activations,activations))
-        new_indices = torch.clone(selected_indices[wanted_indexes])
-        new_indices[:,:,0] = counter
-        all_indices = torch.cat((all_indices,new_indices))
-    all_activations = all_activations.squeeze(1)
-    all_indices = all_indices.squeeze(1)
-    sentences = all_tokens[unique_top_sentence_idx.int()]
+    for counter,idx in enumerate(top_activation_indices):
+        t_features = sentence_features[idx]
+        t_indices = sentence_indices[idx].clone()
+        all_activations = torch.cat((all_activations,t_features))
+        sentence = all_tokens[t_indices[0,0].int()]
+        sentences = torch.cat((sentences,sentence))
+        t_indices[:,0] = counter
+        all_indices = torch.cat((all_indices,t_indices))
+    sentences = sentences.reshape(max_selection,-1)
+
     return sentences, all_activations, all_indices
+
--- scripts/collect_features_data.py
+++ scripts/collect_features_data.py
@@ -1,132 +1,148 @@
 import torch
-from transformer_lens import utils, HookedTransformer
-import datasets
+from transformer_lens import  HookedTransformer
+
 import tqdm
 from argparse import ArgumentParser
-from sae_auto_interp.models.load import get_autoencoder
-import os
+from sae_auto_interp.autoencoders.model import get_autoencoder
+from sae_auto_interp.features.dataset import get_available_configs, get_batches, get_config
 from pathlib import Path
-token = os.getenv("HF_TOKEN")
+
+import json
 
 if __name__ == "__main__":
     parser = ArgumentParser()
+    #TODO: Allow for a list of layers
     parser.add_argument(
         "--layer",
-        type=int, choices=list(range(12))+[-1],default=-1,
+        type=int,
+        default=-1,
+        help="Layer to collect features from, if -1 will collect from all layers",
     )
     parser.add_argument(
         "--model",
-        type=str, default="gpt2",
+        type=str,
+        default="gpt2",
+        choices="gpt2,meta-llama/Meta-Llama-3-8B"
     )
     parser.add_argument(
         "--device",
-        type=str, default="cuda:0",
+        type=str,
+        default="cuda:0",
     )
     parser.add_argument(
         "--number_features",
-        type=int, default=131072,
+        type=int,
+        default=-1,
+        help="Number of features to collect, if -1 will collect all features",
     )
     parser.add_argument(
-        "--number_examples",
-        type=int, default=1000,
+        "--dataset_configuration",
+        type=str,
+        choices=get_available_configs(),
     )
-    
+    parser.add_argument(
+        "--file_suffix",
+        type=str,
+        default="",
+        help="Suffix to add to the file name",
+    )
+
     args = parser.parse_args()
     model_name = args.model
     device = args.device
     layer = args.layer
     number_features = args.number_features
     number_examples = args.number_examples
+    config = args.dataset_configuration
+    config = get_config(config)
+    prefix = args.prefix
 
-    model_name = "meta-llama/Meta-Llama-3-8B"
-    number_features = 100
-    layer = 0
-    #model = AutoModelForCausalLM.from_pretrained(model_name,cache_dir="/mnt/ssd-1/hf_cache/hub",token=token)
-    
+
     print("Loading the model")
-    model = HookedTransformer.from_pretrained(model_name, center_writing_weights=False,device=device)
+    model = HookedTransformer.from_pretrained(
+        model_name, center_writing_weights=False, device=device, dtype="bfloat16"
+    )
 
     print("Loading the autoencoders")
     if layer == -1:
         layers = list(range(12))
-        autoencoders = {layer:get_autoencoder(model_name,layer,device) for layer in layers}
+        autoencoders = {
+            layer: get_autoencoder(model_name, layer, device) for layer in layers
+        }
     else:
         layers = [layer]
-        autoencoders = {layer:get_autoencoder(model_name,layer,device)}
-    
+        autoencoders = {layer: get_autoencoder(model_name, layer, device)}
 
-    print("Loading dataset")
-    ## This is hardcoded for now but I should make a dataset loader
-    dataset = datasets.load_dataset("HuggingFaceFW/fineweb", name="sample-10BT", split="train")
-    
-    
-    keep_examples = number_examples
-    dataset = dataset.select(range(keep_examples))
-    tokenized_data = utils.tokenize_and_concatenate(dataset, model.tokenizer, max_length=64)
-    all_tokens = tokenized_data["tokens"]
-
-    ## This is hardcoded because this is what can fit in our gpus, can make this dynamic later
-    batch_size = 8  
-    mini_batches = all_tokens.split(batch_size)
-    mini_batches = [batch for batch in mini_batches]
-    mini_batches = mini_batches[:-1]
-
-
+    mini_batches = get_batches(config)
+    batch_size = config["batch_size"]
     ## I think we always want all the features, this is more of a debug option
     number_features = number_features
     feature_indices = list(range(number_features))
-    
-    all_non_zero_features= {layer:[] for layer in layers}
-    all_indices = {layer:[] for layer in layers}
-    for minibatch in tqdm.tqdm(mini_batches):
-            with torch.no_grad():
-                _,model_acts = model.run_with_cache(minibatch, remove_batch_dim=False)
-                for layer in layers:
-                    layer_acts = model_acts[f"blocks.{layer}.hook_resid_post"]
-                    #Get the features of the autoencoder
-                    features = autoencoders[layer].encode(layer_acts)
-                    if number_features == 131072:
-                        wanted_features = features
-                    else:
-                        wanted_features = features[:,:,feature_indices]
-                    non_zero_features = wanted_features[wanted_features.abs()>1e-5]
-                    indices = torch.nonzero(wanted_features.abs()>1e-5)
-                    all_non_zero_features[layer].append([non_zero_features.cpu()])
-                    all_indices[layer].append([indices.cpu()])
-                    #Trying to have the memory not blow up
-                    del(layer_acts)
-                    del(features)
-                    del(wanted_features)
-                    del(non_zero_features)
-                    del(indices)
-                del(_)
-                del(model_acts)
-            torch.cuda.empty_cache()
 
+    all_non_zero_features = {layer: [] for layer in layers}
+    all_indices = {layer: [] for layer in layers}
+    for minibatch in tqdm.tqdm(mini_batches):
+        with torch.no_grad():
+            
+            #TODO: Cache only the activations for the layers we are interested in
+            _, model_acts = model.run_with_cache(minibatch, remove_batch_dim=False)
+            for layer in layers:
+                layer_acts = model_acts[f"blocks.{layer}.hook_resid_post"]
+                # Get the features of the autoencoder
+                features = autoencoders[layer](layer_acts)
+                if number_features == -1:
+                    wanted_features = features
+                else:
+                    wanted_features = features[:, :, feature_indices]
+                non_zero_features = wanted_features[wanted_features.abs() > 1e-5]
+                indices = torch.nonzero(wanted_features.abs() > 1e-5)
+                all_non_zero_features[layer].append([non_zero_features.cpu()])
+                all_indices[layer].append([indices.cpu()])
+
+                # Trying to have the memory not blow up
+                del layer_acts
+                del features
+                del wanted_features
+                del non_zero_features
+                del indices
+            del _
+            del model_acts
+        torch.cuda.empty_cache()
 
     if "/" in model_name:
-        model_name = model_name.split("/")[1]    
+        save_name = model_name.split("/")[1]
 
-    feature_path = (
-        Path("features")
-        / model_name
-    )
+    # Make the folder to save the features
+    feature_path = Path("saved_features") / model_name
     feature_path.mkdir(parents=True, exist_ok=True)
 
     for layer in layers:
-        feature_layers =  torch.stack([torch.cat(sub_list, dim=0) for sub_list in all_non_zero_features[layer]], dim=0)
-        indice_layers =  torch.stack([torch.cat(sub_list, dim=0) for sub_list in all_indices[layer]], dim=0)
+
+        # Stacking the features and indices
+        feature_layers = torch.stack(
+            [torch.cat(sub_list, dim=0) for sub_list in all_non_zero_features[layer]],
+            dim=0,
+        )
+        indice_layers = torch.stack(
+            [torch.cat(sub_list, dim=0) for sub_list in all_indices[layer]], dim=0
+        )
         feature_layers = feature_layers.ravel()
-        new_indices = torch.zeros(indice_layers.shape[0]*indice_layers.shape[1],indice_layers.shape[2])
+        new_indices = torch.zeros(
+            indice_layers.shape[0] * indice_layers.shape[1], indice_layers.shape[2]
+        )
         for i in range(indice_layers.shape[0]):
             old_indices = indice_layers[i]
-            old_indices[:,0] = old_indices[:,0]+i*batch_size
-            new_indices[i*indice_layers.shape[1]:(i+1)*indice_layers.shape[1]] = old_indices
-        
-        filename = feature_path+f"/layer_{layer}_features.pt"
+            old_indices[:, 0] = old_indices[:, 0] + i * batch_size
+            new_indices[
+                i * indice_layers.shape[1] : (i + 1) * indice_layers.shape[1]
+            ] = old_indices
+
+        filename = feature_path / f"{prefix}_layer_{layer}_features.pt"
         with open(filename, mode="wb") as f:
             torch.save(feature_layers, f)
-        filename = f"features_llama/layer_{layer}_indices.pt"
+        filename = feature_path / f"{prefix}_layer_{layer}_indices.pt"
         with open(filename, mode="wb") as f:
             torch.save(new_indices, f)
-    
+        filename = feature_path / f"{prefix}_layer_{layer}_config.json"
+        with open(filename, mode="w") as f:
+            json.dump(config, f)
