356af7a3f5194cde697f87034adc9ffa69f1ae0b
SrGonao <g-spaulo@hotmail.com>
2024-07-01 21:12:10 +1000

    Temporary support

--- sae_auto_interp/autoencoders/ae.py
+++ sae_auto_interp/autoencoders/ae.py
@@ -1,24 +1,56 @@
-from typing import Dict
+from typing import Dict, List
 import torch
 from nnsight.editing import Edit
 
-from .OpenAI.model import Autoencoder
-
+from .OpenAI.model import (
+    Autoencoder,
+    TopK,
+    ACTIVATIONS_CLASSES
+)
+from .EleutherAI.model import Sae
 class AutoencoderLatents(torch.nn.Module):
-    def __init__(self, ae):
-        super().__init__()
-        self.ae = ae
-
-    def forward(self, x):
-        latents, _  = self.ae.encode(x)
-        return latents
-
-def load_autoencoders(model, weight_dir) -> Dict[int, Autoencoder]:
-    ae_layers = [0, 1, 2,3, 4,5, 6,7, 8,9, 10,11]
+    def __init__(self, autoencoder: torch.nn.Module,type: str) -> None:
+            super().__init__()
+            self.autoencoder = autoencoder
+            self.n_features = autoencoder.encoder.out_features
+            self.type = type
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        if self.type == "oai":
+            # The OAI autoencoder returns a tuple with the encoded values and statistics, which we don't need
+            latents,_ = self.autoencoder.encode(x)
+            return latents
+        elif self.type == "eai":
+            # The EAI autoencoder returns the encoded values before topk, and we use OAI TopK class.
+            encoded = self.autoencoder.encode(x)
+            trained_k = self.autoencoder.cfg.k
+            topk = TopK(trained_k, postact_fn=ACTIVATIONS_CLASSES["Identity"]())
+            return topk(encoded)
+        
+    
+def load_eai_autoencoders(model,ae_layers: List[int], weight_dir:str):
+    
     ae_dict = {}
     submodule_dict = {}
     edits = []
+    for layer in ae_layers:
+        path = f"{weight_dir}/Meta-LLama-3-8B/layer_{layer}"
+        sae = Sae.load_from_disk(path,"cuda:0")
 
+        submodule = model.transformer.h[layer]
+        passthrough_ae = AutoencoderLatents(sae, "eai")
+        edit = Edit(submodule, "ae", passthrough_ae)
+
+        ae_dict[layer] = sae
+        submodule_dict[layer] = submodule
+        edits.append(edit)
+
+    return ae_dict, submodule_dict, edits	
+
+def load_oai_autoencoders(model,ae_layers: List[int], weight_dir:str):
+    ae_dict = {}
+    submodule_dict = {}
+    edits = []
     for layer in ae_layers:
         path = f"{weight_dir}/resid_post_mlp_autoencoder_{layer}.pt"
         state_dict = torch.load(path)
@@ -26,11 +58,24 @@ def load_autoencoders(model, weight_dir) -> Dict[int, Autoencoder]:
         ae.to("cuda:0")
 
         submodule = model.transformer.h[layer]
-        passthrough_ae = AutoencoderLatents(ae)
+        passthrough_ae = AutoencoderLatents(ae, "oai")
         edit = Edit(submodule, "ae", passthrough_ae)
 
         ae_dict[layer] = ae
         submodule_dict[layer] = submodule
         edits.append(edit)
+
+    return ae_dict, submodule_dict, edits
+
+
+def load_autoencoders(model,ae_layers, weight_dir) -> Dict[int, Autoencoder]:
+    
+    #TODO: We need to make this a little bit differently in the future
+    if "gpt2" in weight_dir:
+        ae_dict, submodule_dict, edits = load_oai_autoencoders(model,ae_layers, weight_dir)
+       
+    if "llama" in weight_dir:
+        ae_dict, submodule_dict, edits = load_eai_autoencoders(model,ae_layers, weight_dir)
+    
 
     return ae_dict, submodule_dict,  edits
