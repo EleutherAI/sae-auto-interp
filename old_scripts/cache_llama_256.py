7a934157933bd2f6c3bc43f70d0b04a71f5e084e
SrGonao <g-spaulo@hotmail.com>
2024-07-12 09:13:29 +0000

    Changing some old scripts

--- old_scripts/cache_llama_256.py
+++ old_scripts/cache_llama_256.py
@@ -1,10 +1,12 @@
+import os
+os.environ["CONFIG_PATH"] = "/mnt/ssd-1/gpaulo/SAE-Zoology/sae_auto_interp/configs/gpaulo_llama_256.yaml"
+
 from sae_auto_interp.autoencoders.ae import load_autoencoders
 from sae_auto_interp.features.cache import FeatureCache
-from sae_auto_interp.utils import get_samples
 from nnsight import LanguageModel
 import torch
 import argparse
-from sae_auto_interp import cache_config as CONFIG
+
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument("--layers", type=str, default="12,14")
@@ -14,21 +16,14 @@ layers = [int(layer) for layer in args.layers.split(",") if layer.isdigit()]
 # Load model and autoencoders
 model = LanguageModel("meta-llama/Meta-Llama-3-8B", device_map="auto", dispatch=True,torch_dtype =torch.bfloat16)
 print("Model loaded")
-# Load autoencoders, submodule dict, and edits.
-# Submodule dict is used in caching to save ae latents
-# Edits are applied to the model
-ae_dict, submodule_dict = load_autoencoders(
+
+submodule_dict = load_autoencoders(
     model, 
     layers,
     "saved_autoencoders/llama-exp32"
 )
 
 print("Autoencoders loaded")
-
-# Get and sort samples
-CONFIG.n_tokens = 10_400_000
-CONFIG.dataset_repo =  "kh4dien/fineweb-100m-sample"
-CONFIG.batch_len = 256
 
 print("Samples loaded")
 # Cache and save features
@@ -37,5 +32,5 @@ cache.run()
 print("Caching complete")
 for layer in layers:
     feature_range = torch.arange(0,10000)
-    cache.save_selected_features(feature_range, layer, save_dir="raw_features_llama")
+    cache.save_selected_features(feature_range, layer, save_dir="raw_features_llama_256")
 print("Selected features saved")
