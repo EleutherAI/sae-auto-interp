10bc02d38aa9f2974c6f08008dd5d963bac4c67d
cadentj <caden.juang@gmail.com>
2024-07-20 20:33:39 -0400

    more fixes

--- test.py
+++ test.py
@@ -20,29 +20,31 @@ class AsyncActor:
 
         print(f"Actor {self.name} processing")
         await asyncio.sleep(1) # Replace this with some processing code.
+        print(f"Actor {self.name} processed")
 
         return result
 
 start = time.time()
-result_ids = [do_some_work.remote(x) for x in range(4)]
-
+result_ids = [do_some_work.remote(x) for x in range(20)]
 
 results = []
 
-actor = AsyncActor.remote("one")
+actor = AsyncActor.options(max_concurrency=4).remote("one")
 
 actor_two = AsyncActor.options(max_concurrency=4).remote("two")
 
+print(len(result_ids))
+
 while len(result_ids):
 
-    done_id, result_ids = ray.wait(result_ids)
+    done_id, result_ids = ray.wait(result_ids, num_returns=4)
 
-    result = actor.process_incremental.remote(ray.get(done_id[0]))
+    result = [actor.process_incremental.remote(i) for i in ray.get(done_id)]
 
-    result_two = actor_two.process_incremental.remote(result)
+    result_two = [actor_two.process_incremental.remote(i) for i in ray.get(result)]
 
     results.append(result_two)
 
-results = ray.get(results)
+r = [ray.get(i)for i in results]
 
-print("duration =", time.time() - start, "\nresult = ", results)
+print("duration =", time.time() - start, "\nresult = ", r)
