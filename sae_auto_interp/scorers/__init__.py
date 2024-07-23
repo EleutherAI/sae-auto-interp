d0cb43856c2d9397daea8ed95ca6081bd970d7f5
cadentj <caden.juang@gmail.com>
2024-07-22 20:25:23 -0400

    simulation

--- sae_auto_interp/scorers/__init__.py
+++ sae_auto_interp/scorers/__init__.py
@@ -1,7 +1,7 @@
 from .scorer import Scorer
 
 from .classifier.recall import RecallScorer
-# from .simulator.oai_simulator import OpenAISimulator
+from .simulator.oai_simulator import OpenAISimulator
 from .classifier.fuzz import FuzzingScorer
 # from .neighbor.neighbor import NeighborScorer
 from .generation.generation import GenerationScorer
