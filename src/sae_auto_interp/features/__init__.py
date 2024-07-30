bd15be467e71ce92774466e0f42ddca189cf03d9
SrGonao <g-spaulo@hotmail.com>
2024-07-30 12:28:31 +0000

    QOL

--- src/sae_auto_interp/features/__init__.py
+++ src/sae_auto_interp/features/__init__.py
@@ -5,6 +5,6 @@ from .loader import FeatureDataset, FeatureLoader
 
 from .constructors import pool_max_activation_windows, random_activation_windows
 
-from .samplers import top_and_quantiles, top_and_activation_quantiles, random_and_quantiles
+from .samplers import top_and_quantiles, top_and_activation_quantiles,random_and_quantiles, random_sample, quantiles_sample, top_sample
 
 from .stats import unigram, get_neighbors
