2f7fc018658a10fabc116037633dbf2c5d0b3f1f
SrGonao <g-spaulo@hotmail.com>
2024-07-31 14:24:39 +0000

    Ruff

--- sae_auto_interp/autoencoders/__init__.py
+++ sae_auto_interp/autoencoders/__init__.py
@@ -1,4 +1,11 @@
 from .EleutherAI import load_eai_autoencoders
+from .Neurons import load_llama3_neurons
 from .OpenAI import load_oai_autoencoders
 from .Sam import load_sam_autoencoders
-from .Neurons import load_llama3_neurons
+
+__all__ = [
+    "load_eai_autoencoders",
+    "load_llama3_neurons",
+    "load_oai_autoencoders",
+    "load_sam_autoencoders",
+]
