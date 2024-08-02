f726626c17f8ba423ab4dc871472ef91bb34db51
SrGonao <g-spaulo@hotmail.com>
2024-08-02 20:43:22 +0000

    Ruff

--- sae_auto_interp/features/__init__.py
+++ sae_auto_interp/features/__init__.py
@@ -1,5 +1,9 @@
 from .cache import FeatureCache
-from .constructors import pool_max_activation_windows, random_activation_windows
+from .constructors import (
+    default_constructor,
+    pool_max_activation_windows,
+    random_activation_windows,
+)
 from .features import Example, Feature, FeatureRecord
 from .loader import FeatureDataset
 from .samplers import sample
@@ -13,6 +17,7 @@ __all__ = [
     "Example",
     "pool_max_activation_windows",
     "random_activation_windows",
+    "default_constructor",
     "sample",
     "get_neighbors",
     "unigram",
