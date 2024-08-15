76bf4c0fb67dfc2a33c5b5fc807fc385b1d8b903
SrGonao <g-spaulo@hotmail.com>
2024-08-15 10:25:12 +0000

    Random loader + train quantiles

--- sae_auto_interp/explainers/explainer.py
+++ sae_auto_interp/explainers/explainer.py
@@ -1,6 +1,8 @@
+import json
+import os
+import random
 from abc import ABC, abstractmethod
 from typing import NamedTuple
-import json
 
 import aiofiles
 
@@ -30,4 +32,15 @@ async def explanation_loader(record: FeatureRecord, explanation_dir: str) -> Exp
         explanation=explanation
     )
 
-    return ExplainerResult(record=record, explanation=explanation)
+async def random_explanation_loader(record: FeatureRecord, explanation_dir: str) -> ExplainerResult:
+    explanations = [f for f in os.listdir(explanation_dir) if f.endswith(".txt")]
+    if str(record.feature) in explanations:
+        explanations.remove(str(record.feature))
+    random_explanation = random.choice(explanations)
+    async with aiofiles.open(f'{explanation_dir}/{random_explanation}', 'r') as f:
+        explanation = json.loads(await f.read())
+    
+    return ExplainerResult(
+        record=record,
+        explanation=explanation
+    )
