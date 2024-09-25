cf3e2c9ca4e4272824e0ed97e582703ac74c86cc
Alex Mallen <atmallen8@gmail.com>
2024-09-25 19:10:33 +0000

    random sae caching

--- sae_auto_interp/autoencoders/__init__.py
+++ sae_auto_interp/autoencoders/__init__.py
@@ -1,12 +1,14 @@
 from .Neurons import load_llama3_neurons
 from .OpenAI import load_oai_autoencoders
 from .Sam import load_sam_autoencoders
-from .DeepMind import load_gemma_autoencoders, load_gemma_topk_latents
+from .DeepMind import load_gemma_autoencoders
+from .eleuther import load_eai_autoencoders, load_random_eai_autoencoders
 
 __all__ = [
     "load_gemma_autoencoders",
     "load_llama3_neurons",
     "load_oai_autoencoders",
     "load_sam_autoencoders",
-    "load_gemma_topk_latents",
+    "load_eai_autoencoders",
+    "load_random_eai_autoencoders",
 ]
