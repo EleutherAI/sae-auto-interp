39581cce2324b9ecc6298d38c7e4d442fea64bc3
Alex Mallen <atmallen8@gmail.com>
2024-10-01 05:37:12 +0000

    fix random expl scoring script

--- random_expl_sweep.py
+++ random_expl_sweep.py
@@ -1,6 +1,6 @@
 import itertools
 
-base_cmd = "python /mnt/ssd-1/alexm/sae-auto-interp/counterfactual_pipeline.py "
+base_cmd = "python /mnt/ssd-1/alexm/sae-auto-interp/run_score_with_random_expls.py "
 
 cfg_ranges = {
     "n_feats": [300],
@@ -10,7 +10,7 @@ cfg_ranges = {
     "n_explanations": [5],
     "kl_threshold": [0.1, 0.33, 1.0, 3.0],
     "random_explanations": [False],
-    "latents": ["sae", "random"],
+    "latents": ["sae"],
     # "zero_ablate": [True],
 }
 
@@ -19,15 +19,11 @@ cfgs = [dict(zip(keys, v)) for v in itertools.product(*values)]
 
 for cfg in cfgs:
     cmd = base_cmd 
-    for k, v in cfg.items():
-        if isinstance(v, bool):
-            if v:
-                cmd += f" --{k} "
-        else:
-            cmd += f" --{k} {v} "
     run_name = "_".join([
         "".join([word[0] for word in k.split("_")]) + "=" + str(v)
         for k, v in cfg.items()
     ])
-    cmd += f" --run_prefix \"{run_name}\" "
+    run_path = f"/mnt/ssd-1/alexm/sae-auto-interp/counterfactual_results/{run_name}/generations.json"
+    cmd += f" --expl_path \"{run_path}\" "
+    cmd += " --explainer_name \"meta-llama/Meta-Llama-3.1-8B\" "
     print(cmd)
--- sae_auto_interp/counterfactuals/score_with_random_expl.py
+++ sae_auto_interp/counterfactuals/score_with_random_expl.py
@@ -29,17 +29,18 @@ def load_explainer(explainer_name, device):
 
     return explainer, explainer_tokenizer
 
-def main(expl_path: Path | str, explainer_name: str, device: str):
-    # NOTE: this is kind of a hack
-    save_path = Path(str(expl_path).replace("re=True", "re=False").replace("generations.json", "generations_scores.json"))
+
+def main(expl_path: Path | str, explainer_name: str = "meta-llama/Meta-Llama-3.1-8B", device: str = "cuda"):
+    save_path = Path(str(expl_path).replace("re=False", "re=True"))
     random_explanations_df = pd.read_json(expl_path)
     all_expls = [e for l in random_explanations_df["explanations"].tolist() for e in l]
-    print(f"{Counter(all_expls)=}")
     if Counter(all_expls).most_common(1)[0][1] / len(all_expls) > 0.25:
         warnings.warn("More than 25% of the explanations are the same. Shuffling may not cause enough change in the explanations.")
 
     # shuffle the explanations column
     random_explanations_df["explanations"] = random.sample(random_explanations_df["explanations"].tolist(), k=len(random_explanations_df))
+    save_path.parent.mkdir(parents=True, exist_ok=True)
+    random_explanations_df.to_json(save_path, orient="records")
 
     explainer, explainer_tokenizer = load_explainer(explainer_name, device)
 
