acee5cb1a0e077a169a5efd43a03e8aa02f5bb03
SrGonao <g-spaulo@hotmail.com>
2024-10-14 10:11:52 +0000

    Add experiment code

---
+++ experiments/caching_gemma/cache_neurons.py
@@ -0,0 +1,65 @@
+from nnsight import LanguageModel
+from simple_parsing import ArgumentParser
+
+from sae_auto_interp.autoencoders import load_llama3_neurons
+from sae_auto_interp.config import CacheConfig
+from sae_auto_interp.features import FeatureCache
+from sae_auto_interp.utils import load_tokenized_data
+import os
+
+def main(cfg: CacheConfig,args): 
+    model = LanguageModel("google/gemma-2-9b", device_map="cuda", dispatch=True,torch_dtype="float16")
+    layers = args.layers
+    k=args.k
+    layers = [int(layer) for layer in layers.split(",")]
+    
+    submodule_dict,model = load_llama3_neurons(
+        model,
+        layers,
+        args.k
+    )
+    
+
+    tokens = load_tokenized_data(
+        cfg.ctx_len,
+        model.tokenizer,
+        cfg.dataset_repo,
+        cfg.dataset_split,
+        cfg.dataset_name    
+    )
+
+    cache = FeatureCache(
+        model, 
+        submodule_dict, 
+        batch_size=cfg.batch_size,
+    )
+
+    cache.run(cfg.n_tokens, tokens)
+
+    if not os.path.exists(f"raw_features/gemma/neurons_{k}"):
+        os.makedirs(f"raw_features/gemma/neurons_{k}")
+
+    cache.save_splits(
+        n_splits=cfg.n_splits, 
+        save_dir=f"raw_features/gemma/neurons_{k}"
+    )
+
+    cache.save_config(
+        save_dir=f"raw_features/gemma/neurons_{k}",
+        cfg=cfg,
+        model_name="google/gemma-2-9b"
+    )
+
+
+if __name__ == "__main__":
+
+    parser = ArgumentParser()
+    #ctx len 256
+    parser.add_arguments(CacheConfig, dest="options")
+    parser.add_argument("--k", type=int, default=32)
+    parser.add_argument("--layers", type=str, default="23,27")
+    
+    args = parser.parse_args()
+    cfg = args.options
+
+    main(cfg,args)
---
+++ experiments/caching_llama/cache.py
@@ -0,0 +1,82 @@
+from nnsight import LanguageModel
+from simple_parsing import ArgumentParser
+import torch
+from sae_auto_interp.autoencoders import load_eai_autoencoders
+from sae_auto_interp.config import CacheConfig
+from sae_auto_interp.features import FeatureCache
+from sae_auto_interp.utils import load_tokenized_data
+import os
+
+def main(cfg: CacheConfig, args): 
+
+    size = args.size
+    randomize = args.randomize
+    k = args.k
+    type = args.type
+
+    model = LanguageModel("meta-llama/Meta-Llama-3.1-8B", device_map="auto",dispatch=True,torch_dtype=torch.bfloat16)
+    
+    if size == "32x":
+        weight_dir = "EleutherAI/sae-llama-3.1-8b-32x"
+    elif size == "64x":
+        weight_dir = "EleutherAI/sae-llama-3.1-8b-64x"
+    elif size == "64x_no_multi":
+        weight_dir = "/mnt/ssd-1/nora/llama-64x-no-multitopk"
+
+    
+    submodule_dict,model = load_eai_autoencoders(
+        model,
+        [23,29],
+        weight_dir,
+        module=type,
+        randomize=randomize,
+        k=k
+    )
+    
+
+    tokens = load_tokenized_data(
+        cfg.ctx_len,
+        model.tokenizer,
+        cfg.dataset_repo,
+        cfg.dataset_split,
+    )
+    print(submodule_dict)
+    cache = FeatureCache(
+        model, 
+        submodule_dict, 
+        batch_size=cfg.batch_size,
+    )
+    name=""
+    if k is not None:
+        name += f"_topk_{k}"
+    if randomize:
+        name += "_random"
+    if type != "res":
+        name += f"_{type}"
+    cache.run(10000000, tokens)
+    os.makedirs(f"raw_features/llama/{size}{name}", exist_ok=True)
+
+    cache.save_splits(
+        n_splits=cfg.n_splits, 
+        save_dir=f"raw_features/llama/{size}{name}"
+    )
+    cache.save_config(
+        save_dir=f"raw_features/llama/{size}{name}",
+        cfg=cfg,
+        model_name="meta-llama/Meta-Llama-3.1-8B"
+    )
+
+if __name__ == "__main__":
+
+    parser = ArgumentParser()
+    #ctx len 256
+    parser.add_arguments(CacheConfig, dest="options")
+    parser.add_argument("--size", type=str, default="32x")
+    parser.add_argument("--type", type=str, default="res")
+    parser.add_argument("--randomize", action="store_true")
+    parser.add_argument("--k", type=int, default=None)
+    args = parser.parse_args()
+    cfg = args.options
+    print(cfg)
+    
+    main(cfg, args)
