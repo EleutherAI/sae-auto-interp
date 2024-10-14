74a3095a647815ce983cd7643a384c5a96eff755
SrGonao <g-spaulo@hotmail.com>
2024-10-14 09:16:01 +0000

    Removing unused code

--- sae_auto_interp/autoencoders/DeepMind/__init__.py
+++ sae_auto_interp/autoencoders/DeepMind/__init__.py
@@ -8,25 +8,14 @@ DEVICE = "cuda:0"
 
 
 
-def load_gemma_autoencoders(model, ae_layers: list[int],average_l0s: Dict[int,int],size:str,type:str,randomize:bool=False):
+def load_gemma_autoencoders(model, ae_layers: list[int],average_l0s: Dict[int,int],size:str,type:str):
     submodules = {}
 
     for layer in ae_layers:
-        if randomize:
-            d_model = model.config.hidden_size 
-            d_sae = 131072
-            sae = JumpReLUSAE(d_model,d_sae)
-            #Randomize the weights
-            sae.W_enc.data.uniform_(-1,1)
-            sae.W_dec.data.uniform_(-1,1)
-            # This does not work
-            sae.threshold.data.uniform_(-1,1)
-            sae.b_enc.data.uniform_(-1,1)
-            sae.b_dec.data.uniform_(-1,1)
-        else:
-            path = f"layer_{layer}/width_{size}/average_l0_{average_l0s[layer]}"
-            sae = JumpReLUSAE.from_pretrained(path,type,"cuda")
-            
+    
+        path = f"layer_{layer}/width_{size}/average_l0_{average_l0s[layer]}"
+        sae = JumpReLUSAE.from_pretrained(path,type,"cuda")
+        
         sae.half()
         def _forward(sae, x):
             encoded = sae.encode(x)
--- sae_auto_interp/oai_autointerp/__init__.py
+++
@@ -1,9 +0,0 @@
-from .explanations import (
-    simulate_and_score, 
-    LogprobFreeExplanationTokenSimulator,
-    ExplanationNeuronSimulator,
-)
-
-from .activations.activations import (
-    ActivationRecord
-)
