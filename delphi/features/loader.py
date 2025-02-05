10a6232c4cfd1ef3bb95c25cb987488b6dd4033f
Lucia Quirke <luciarosequirke@gmail.com>
2025-02-05 07:55:11 +0000

    Rename library

--- delphi/features/loader.py
+++ delphi/features/loader.py
@@ -10,7 +10,7 @@ from safetensors.numpy import load_file
 from torchtyping import TensorType
 from tqdm import tqdm
 
-from sae_auto_interp.utils import (
+from delphi.utils import (
     load_tokenized_data,
 )
 
