c3d1d972e64847693f9131e4f19769e34f94ecb0
SrGonao <g-spaulo@hotmail.com>
2025-02-10 15:53:45 +0000

    Correctly create neighbours in transform

--- delphi/features/transforms.py
+++ delphi/features/transforms.py
@@ -2,19 +2,31 @@ from typing import Callable, Optional
 
 import torch
 from torchtyping import TensorType
-
+from dataclasses import dataclass
 from .features import FeatureRecord, prepare_examples
 from .loader import BufferOutput
 
 import json
+
+
+@dataclass
+class Neighbour:
+    distance: float
+    feature_index: int
+
 def set_neighbours(
     record: FeatureRecord,
-    neighbours_path: str,
-    neighbours_type: str,
+    neighbours: dict[int, list[tuple[float, int]]],
+    threshold: float,
 ):
     """
     Set the neighbours for the feature record.
     """
-    with open(neighbours_path, "r") as f:
-        neighbours = json.load(f)
-    record.neighbours = neighbours[neighbours_type][record.feature.feature_index]
+    
+    neighbours = neighbours[str(record.feature.feature_index)]
+
+    # Each element in neighbours is a tuple of (distance,feature_index)
+    # We want to keep only the ones with a distance less than the threshold
+    neighbours = [neighbour for neighbour in neighbours if neighbour[0] > threshold]
+
+    record.neighbours = [Neighbour(distance=neighbour[0], feature_index=neighbour[1]) for neighbour in neighbours]
